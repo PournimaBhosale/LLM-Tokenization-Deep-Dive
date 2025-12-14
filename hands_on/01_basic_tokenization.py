7832eac5fc841171e80ec12106d01ba63da4dcc9
Pournima Bhosale <159861311+PournimaBhosale@users.noreply.github.com>
2025-12-14 12:27:46 +0530

    Create 01_basic_tokenization.py

---
+++ hands_on/01_basic_tokenization.py
@@ -0,0 +1,11 @@
+# 01_basic_tokenization.py
+
+text = "Hello world! Tokenization is fun."
+
+# Word Tokenization
+word_tokens = text.split()
+print("Word Tokens:", word_tokens)
+
+# Character Tokenization
+char_tokens = list(text)
+print("Character Tokens:", char_tokens)

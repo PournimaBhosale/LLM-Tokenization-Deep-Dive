ede05eae59b5d7525bf95775d45f139229b17b23
Pournima Bhosale <159861311+PournimaBhosale@users.noreply.github.com>
2025-12-14 12:28:48 +0530

    Update 02_subword_tokenization_demo.py

--- hands_on/02_subword_tokenization_demo.py
+++ hands_on/02_subword_tokenization_demo.py
@@ -1,1 +1,14 @@
+# 02_subword_tokenization_demo.py
 
+words = ["unhappiness", "tokenization", "preprocessing"]
+
+# Simple manual subword split (demo purpose)
+subword_tokens = []
+for word in words:
+    if word.startswith("un"):
+        subword_tokens.append("un")
+        subword_tokens.append(word[2:])
+    else:
+        subword_tokens.append(word)
+
+print("Subword Tokens:", subword_tokens)

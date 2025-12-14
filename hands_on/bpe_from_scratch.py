a46bc5137538b20a51b9ba71f97a692812cc8ef4
Pournima Bhosale <159861311+PournimaBhosale@users.noreply.github.com>
2025-12-14 11:22:53 +0530

    Implement get_stats function for BPE

---
+++ hands_on/bpe_from_scratch.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def get_stats(vocab):
+    pairs = Counter()
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range(len(symbols) - 1):
+            pairs[(symbols[i], symbols[i + 1])] += freq
+    return pairs

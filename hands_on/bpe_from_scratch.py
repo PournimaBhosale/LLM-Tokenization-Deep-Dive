6c3ca2f7d08253c2bbcf73043024284714eee28f
Pournima Bhosale <159861311+PournimaBhosale@users.noreply.github.com>
2025-12-14 12:28:29 +0530

    Delete hands_on/bpe_from_scratch.py

--- hands_on/bpe_from_scratch.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-def get_stats(vocab):
-    pairs = Counter()
-    for word, freq in vocab.items():
-        symbols = word.split()
-        for i in range(len(symbols) - 1):
-            pairs[(symbols[i], symbols[i + 1])] += freq
-    return pairs
